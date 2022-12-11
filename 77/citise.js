e5a0e4219640c0a457b5184af91663f3ca7dd8d0
MS776 <106539276+MS776@users.noreply.github.com>
2022-12-12 01:50:40 +0800

    Add files via upload

---
+++ 77/citise.js
@@ -0,0 +1,66 @@
+const cities = [
+    {
+        city: "New York",
+        population: "8405837",
+    },
+    {
+        city: "Nyw York",
+        population: "8405837",
+    },
+    {
+        city: "Naw York",
+        population: "8405837",
+    },
+    {
+        city: "Los Angeles",
+        population: "3884307",
+    },
+    {
+        city: "Lbs Angeles",
+        population: "3884307",
+    },
+    {
+        city: "Lss Angeles",
+        population: "3884307",
+    },
+    {
+        city: "纽约",
+        population: "8537673",
+    },
+    {
+        city: "洛杉矶",
+        population: "3976322",
+    },
+    {
+        city: "芝加哥",
+        population: "2704958",
+    },
+    {
+        city: "休斯顿",
+        population: "2303482",
+    },
+    {
+        city: "菲尼克斯",
+        population: "1615017",
+    },
+    {
+        city: "费城",
+        population: "1567872",
+    },
+    {
+        city: "圣安东尼奥",
+        population: "1492510",
+    },
+    {
+        city: "圣迭戈",
+        population: "1406630",
+    },
+    {
+        city: "达拉斯",
+        population: "1317929",
+    },
+    {
+        city: "圣荷西",
+        population: "1025350",
+    },
+];
